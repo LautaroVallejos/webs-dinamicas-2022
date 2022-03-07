e6691a8c119ae8ae76df5ea43d58cf933d60ee1c
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-03-07 20:02:29 -0300

    Primera clase implementacion de webs dinamicas | Formularios

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    // $name = $_POST['name'];
+    // $age = $_POST['age'];
+    // $email = $_POST['mail'];
+    // $dni = $_POST['dni'];
+
+     $name = $_GET['name'];
+    $age = $_GET['age'];
+    $email = $_GET['mail'];
+    $dni = $_GET['dni'];
+    echo "nombre es $name, la edad es $age, el mail es $email, el dni es $dni";
+
+?>
