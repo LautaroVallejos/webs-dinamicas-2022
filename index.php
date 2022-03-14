5445ed03e728db2e01cf8c1d813a097683147f55
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-03-14 18:41:49 -0300

    arreglando problemas

--- index.php
+++ index.php
@@ -4,55 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="articles.css">
+    <link rel="stylesheet" href="./main.css">
     <title>Calculador de supermercado</title>
 </head>
 <body>
     <!-- Contenedor del Formulario -->
     <div class="container">
-        <form class='forms' action="#" method="post">
-            <h1><i>Compra de Articulos</i></h1>
-            <div class="in-container">
-                <label for="">Producto</label>
-                <input class="inputf" type="text" name="product" placeholder="Ingrese producto">
+        <form action="result.php" method="post">
+
+            <div class="section">
+                <label class="label" for="">Titulo de la Página</label>
+                <input type="text" name="title" id="title-text" placeholder="Ingrese un titulo">
+            </div>
+            
+            <div class="section">
+                <label for="" class="label">Encabezado</label>
+                <input type="number" name="header" id="header-number" placeholder="Encabezado del 1 - 6">
+            </div>
+            
+            <div class="section">
+                <label for="" class="label">Texto</label>
+                <textarea name="text-area" id="text-input" placeholder="Ingrese un texto" cols="30" rows="10"></textarea>
             </div>
 
-            <div class="in-container">
-                <label for="">Cantidad</label>
-                <input class="inputf" type="number" name="quantity" placeholder="Ingrese cantidad">
+            <div class="section">
+                <input type="submit" value="Enviar" name="send" class="send">
             </div>
-
-            <div id="submit-button" class="in-container">
-               <input class="button" type="submit" name="send" value="Calcular Total">
-            </div>
+        
         </form>
     </div>
 
-    <!-- Instrucciones Dinamicas -->
-    <?php
-        if(isset($_POST['send'])){
-            //Productos en Stock
-            $products = array("yerba" => 200, "mayonesa" => 100, "gaseosa" => 120, "don satur" => 80);
-            $product_exist = 0;
-
-            foreach($products as $product => $price){
-            // Si hace match con un producto calcula el tota y lo devuelve
-                if($_POST['product'] == $product && isset($_POST['quantity'])){
-                    $cant = $_POST['quantity'];
-                    $total = $price * $cant;
-                    echo "El total es de: $$total";
-                    $product_exist = 1;
-                    break; // Una vez encuentra el producto corta del foreach
-                }
-            }
-
-            // Si el producto existe retorna el total por pantalla, sino devuelve error
-            if($product_exist == 1){
-                    echo "<br>Operacion realizada exitosamente";
-            } else{
-                    echo "Error, el producto no se encuetra diponible o la cantidad no ha sido ingresada";
-            }
-    ?>
+    
 
 </body>
 </html>
