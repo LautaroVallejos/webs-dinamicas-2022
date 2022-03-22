2392f526a73d2768a3510c4b0710bf83f23b6229
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-03-22 18:33:23 -0300

    Trabajo conjunto con fran bot v1

--- result.php
+++ result.php
@@ -1,50 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
-    <title><?php echo $_POST['title'] ?></title>
-</head>
-<body>
-    <!-- Instrucciones Dinamicas -->  
-    <?php
-        $numA = $_REQUEST['numA'];
-        $numB = $_REQUEST['numB'];
-        $opcion = $_REQUEST['op'];
-        $res;
+<?php
+    //Inicializacion de Variables
+    $name = $_REQUEST['name'];
+    $surname = $_REQUEST['surname'];
+    $user = $_REQUEST['user'];
+    $password = $_REQUEST['pass'];
+    $age = $_REQUEST['age'];
+    $sex = $_REQUEST['sex'];
+    $birthday = $_REQUEST['birthday'];
+    $nationality = $_REQUEST['nationality'];
+    $color = $_REQUEST['colour'];
+    $email = $_REQUEST['email'];
+    $description = $_REQUEST['desc'];
+    $hobbies = $_REQUEST['hobbies'];
 
-        if(isset($_POST['send'])){
+    // $send = $_REQUEST['send'];
 
-            if($opcion == 1){
-                $res = $numA + $numB;
-            }
-
-            if($opcion == 2){
-                $res = $numA - $numB;
-            }
-
-            if($opcion == 3){
-                $res = $numA * $numB;
-            }
-
-            if($opcion == 4){
-                if($numB == 0){
-                    $res = "Error, no es posible dividir entre 0.";
-                } else{
-                    $res = $numA / $numB;
-                }
-            }
-
-            switch($opcion){
-                case 1: echo "<h1>El resultado de la operacion es: $res</h1>"; break;
-                case 2: echo "<h1>El resultado de la operacion es: $res</h1>"; break;
-                case 3: echo "<h1>El resultado de la operacion es: $res</h1>"; break;
-                case 4: echo "<h1>El resultado de la operacion es: $res</h1>"; break;
-                default: echo "A ocurrido un error";break;
-            }
-        }
-    ?>
-</body>
-</html>
+    // if(isset($send)){
+        echo "<h1 class='h1'>El nombre seleccionado es $name</h1>";
+        echo "<h1 class='h1'>El apellido seleccionado es $surname</h1>";
+        echo "<h1 class='h1'>El nombre de usuario es $user</h1>";
+        echo "<h1 class='h1'>La contrasena es $password</h1>";
+        echo "<h1 class='h1'>La edad seleccionada es $age</h1>";
+        echo "<h1 class='h1'>El sexo seleciconado es $sex</h1>";
+        echo "<h1 class='h1'>El dia de cumpleanos selecconado es $birthday</h1>";
+        echo "<h1 class='h1'>La nacionalidad seleccionada es $nationality</h1>";
+        echo "<h1 class='h1'>El color favorito seleccionado es $color</h1>";
+        echo "<h1 class='h1'>El email seleccionado es $email</h1>";
+        echo "<h1 class='h1'>La descripcion escrita es $description</h1>";
+    
+    foreach($hobbies as $hobbie){
+        echo "<h1 class='h1'>Los hobbies seleccionados son: $hobbie</h1>";
+    }
+    // }
+?>
