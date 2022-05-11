3b63430b0708cbfb52a3f1c9a778c3f7114ac85b
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-05-11 14:01:40 -0300

    Iniciando Aplicacion CRUD

--- index.php
+++ index.php
@@ -1,34 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./estilos.css">
-    <title>Formulario de imagenes</title>
-</head>
-<body>
-    <!--Formulario-->
-    <form action="slider.php" class="form" method="post" enctype="multipart/form-data">
-        <h1 class="title">Ingrese 3 imagenes</h1>
+<?php require('./db.php')?>
 
-        <div class="input-container">
-            <label for="wall1">Primera Imagen</label>
-            <input type="file" name="foto1" id="wall1">
-        </div>
+<?php include('./includes/header.php')?>
+    
 
-        <div class="input-container">
-            <label for="wall1">Segundas Imagen</label>
-            <input type="file" name="foto2" id="wall2">
-        </div>
-
-        <div class="input-container">
-            <label for="wall1">Tercera Imagen</label>
-            <input type="file" name="foto3" id="wall3">
-        </div>
-
-        <input class="boton" type="submit" value="Enviar Imagenes">
-    </form>
-
-</body>
-</html>
+<?php include('./includes/footer.php')?>
