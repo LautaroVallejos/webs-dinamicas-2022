c4f8667e1d3427607cb525ab445836caf9708269
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-05-17 13:50:39 -0300

    save-tasks.php funcionando

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php include('./includes/header.php')?>
 
-    <?php //require('./db.php')?>
     <?php include('./includes/navbar.php')?>
-
-    <?php include ('./includes/dashboard.php')?>
+    <?php include ('./includes/form.php')?>
 
 <?php include('./includes/footer.php')?>
--- save-tasks.php
+++ save-tasks.php
@@ -1,1 +1,12 @@
-Holiwis
+<?php
+    require_once('db.php');
+    include('./includes/header.php');
+    include('./includes/navbar.php');
+
+        $titleTask = $_POST['title-task'];
+        $descriptionTask = $_POST['description'];
+        
+        $newTask = "INSERT INTO tasks (title, description) VALUES ('$titleTask', '$descriptionTask')";
+        $querySet = mysqli_query($db, $newTask) ? print("<script>alert('Tarea creada correctamente')</script>") : print('Error al realizar la consulta: '.mysqli_error());
+
+    include_once('./includes/footer.php');
