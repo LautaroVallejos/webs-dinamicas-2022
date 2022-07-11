46fdd1c48a4cbf15229a8c41679972aa03e3010a
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-07-11 20:20:15 -0300

    CRUD PHP Terminado

--- delete-tasks.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once('db.php');
-    include('./includes/header.php');
-    include('./includes/navbar.php');
-    include('./includes/delete-form.php');
-    include('./includes/dashboard.php');
-    include('./includes/footer.php');
--- update-tasks.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once('./db.php');
-    include('./includes/header.php');
-        include('./includes/navbar.php');
-        include('./includes/update-item.php');
-    include('./includes/footer.php');
